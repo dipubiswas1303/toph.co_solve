9c97711bc4fd1ebc6362580ace572ebea3ba2bac
Dipu Biswas <dipubiswas1303@gmail.com>
2021-05-03 15:43:27 +0600

    Add Them Up
    https://toph.co/p/add-them-up

---
+++ Add Them Up.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+    /*
+    NAME : DIPU BISWAS
+    JUST CSE 2019 - 2020
+    PROBLEM CODE : Add Them Up
+    LINK : https://toph.co/p/add-them-up
+    */
+
+int main()
+{
+    int a , b;
+    cin >> a >> b;
+    cout << a + b << endl;
+
+    return 0;
+}
